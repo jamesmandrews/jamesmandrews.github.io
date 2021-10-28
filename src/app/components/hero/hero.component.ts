db552e41591e17ea61a5cb8fca54bc0bf700f6f3
James Andrews <jamesmandrews@users.noreply.github.com>
2021-10-29 07:28:23 +0900

    hero parallax works.

--- src/app/components/hero/hero.component.ts
+++ src/app/components/hero/hero.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.scss']
 })
-export class HeroComponent implements OnInit {
+export class HeroComponent implements OnInit, AfterViewInit {
+  @ViewChild('parallax') parallax; 
+  styleBackgroundPosition: string = '0px';
 
-  constructor() { }
+  constructor() { 
+
+  }
 
   ngOnInit(): void {
+    this.styleBackgroundPosition='';
   }
+
+  ngAfterViewInit(): void {
+    window.addEventListener('scroll', () => {
+      const multiplier: number = .7;
+      let offset = window.pageYOffset;
+      this.styleBackgroundPosition = (offset * multiplier).toString() + 'px';
+    });
+    console.log(this.parallax);
+  }
+
 
 }
