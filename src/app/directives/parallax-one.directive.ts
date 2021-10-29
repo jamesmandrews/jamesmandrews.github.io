1ee6f0210cf4d0da93110630d2f90d595fd3fe47
James Andrews <jamesmandrews@users.noreply.github.com>
2021-10-30 05:19:50 +0900

    made multiplier an input of the directive.

--- src/app/directives/parallax-one.directive.ts
+++ src/app/directives/parallax-one.directive.ts
@@ -1,16 +1,17 @@
-import { Directive, AfterViewInit, ElementRef } from '@angular/core';
+import { Directive, AfterViewInit, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appParallaxOne]'
 })
 export class ParallaxOneDirective implements AfterViewInit{
   styleBackgroundPosition: string = '0px';
+  @Input('multiplier') multiplier!: number; 
 
   constructor(private elRef: ElementRef) {}
 
    ngAfterViewInit(): void {
     window.addEventListener('scroll', () => {
-      const multiplier: number = .7;
+      const multiplier: number = this.multiplier;
       let offset = window.pageYOffset;
       this.styleBackgroundPosition = (offset * multiplier).toString() + 'px';
       this.elRef.nativeElement.style.backgroundPositionY=this.styleBackgroundPosition;
